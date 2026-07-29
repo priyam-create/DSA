ee90437a6d51295730ba7c2819a063683d7fa61b
Priyam Verma <priyamverma2025@gmail.com>
2026-07-29 22:47:07 +0530

    Initial commit

---
+++ DSA/inputs01.java
@@ -0,0 +1,11 @@
+package DSA;
+import java.util.Scanner;
+public class inputs01 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            System.out.println(a);
+            
+        }
+    
+}
---
+++ main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        //  double a=22/7.0;
+          //System.out.printf("%.2f",a);
+         // ----------------------------------------
+      //  int a=105+08; 
+      //------------------------------------------------
+      //github profile - dsa folder- commit codes of input of all data types 
+      //bufferreader
+      //--------------------------------------------------------------
+      //Arrays.fill(a,2);
+        
+
+    }
+}
