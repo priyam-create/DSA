ed4f431a21ac87242fbac44a5933aea0693124f4
Priyam Verma <priyamverma2025@gmail.com>
2026-07-29 23:24:07 +0530

    modified code

--- DSA/inputs01.java
+++ DSA/inputs01.java
@@ -4,8 +4,20 @@ public class inputs01 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int a = sc.nextInt();
+            long b = sc.nextLong();
+            short c = sc.nextShort();
+            byte d = sc.nextByte();
+            float e = sc.nextFloat();
+            double f = sc.nextDouble();
+            sc.nextLine();
+            String s = sc.nextLine();
             System.out.println(a);
-            
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+            System.out.println(f);
+            System.out.println(s);
         }
     
 }
