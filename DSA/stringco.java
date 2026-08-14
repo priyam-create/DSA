060128d53b077125a484657af71b70850da661f5
Priyam Verma <priyamverma2025@gmail.com>
2026-08-14 11:44:13 +0530

    Added string manipulation examples and methods in stringco.java

--- DSA/stringco.java
+++ DSA/stringco.java
@@ -17,6 +17,47 @@ public class stringco {
         for(int i=0;i<s2.length();i++){
             System.out.println(s2.charAt(i));
         }
+        char ch='a';
+        int a=ch-32;
+        System.out.println((char)a);
+        
+        String s3="abcd";
+        String s4="abcd";
+        if(s3==s4) System.out.println("same");
+        else System.out.println("Not same");
+
+        System.out.println(s3.equals(s4));
+        String s5="ABcd";
+        String s6="abcd";
+        //toLowerCase();
+        //toUpperCase();
+        System.out.println(s5.equalsIgnoreCase(s6));
+
+        String s7="Hello World";
+        String s8="Hello";
+        System.out.println(s7.startsWith(s8));
+        String s9="World";
+        System.out.println(s7.endsWith(s9));
+        String s10="ello";
+        System.out.println(s7.contains(s10));
+        //.indexOf();
+        //.lastIndexOf();
+        String s11="     Hello    ";
+        System.out.println(s11.trim());
+
+        String s12="abcaadefa";
+        String s13= s12.replace('a','z');
+        //.replaceFirst();
+        System.out.println(s13);
+
+
+        String s14="Abcdef";
+        System.out.println(s14.substring(3,6));
+        System.out.println();
+
+        
+
+
 
     }
 }
